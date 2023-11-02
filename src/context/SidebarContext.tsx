8588da0324b593eb64f06929f75e8735923c2fae
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-11-02 15:53:11 +0700

    `create` sidebar context

---
+++ src/context/SidebarContext.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, createContext, useContext, useState } from 'react';
+
+type SidebarProviderProps = {
+  children: ReactNode;
+};
+type SidebarContext = {
+  expanded: boolean;
+  sidebarToggle: () => void;
+};
+
+const SidebarContext = createContext({} as SidebarContext);
+
+export const useSidebar = () => useContext(SidebarContext);
+
+export const SidebarProvider = ({ children }: SidebarProviderProps) => {
+  const [expanded, setExpanded] = useState(true);
+
+  const sidebarToggle = () => {
+    setExpanded((curr) => !curr);
+  };
+
+  return (
+    <SidebarContext.Provider
+      value={{
+        expanded,
+        sidebarToggle,
+      }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
