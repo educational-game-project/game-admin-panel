494afce31e776d5af2b623c91b0b588b0aa06fe8
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-11-02 15:52:40 +0700

    `add` inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        '2xs': '.625rem',
+      },
+    },
   },
   plugins: [],
 };
