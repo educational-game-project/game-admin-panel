e75605b5c1755417325e6eb677bea66675613cbe
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-11-04 10:41:10 +0700

    `update` app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import Layout from './components/global/Layout';
+import Layout from './common/Layout';
 import Home from './pages/Home';
 import Students from './pages/Students';
 import Score from './pages/Score';
@@ -9,54 +9,58 @@ import Support from './pages/Support';
 import Profile from './pages/Profile';
 import ErrorPage from './pages/Error';
 import Login from './pages/auth/Login';
+import AutoScrollToTop from './components/AutoScrollToTop';
 
 function App() {
   return (
-    <Routes>
-      {/* routes with layout */}
-      <Route
-        path="/"
-        element={<Layout />}>
+    <>
+      <AutoScrollToTop />
+      <Routes>
+        {/* routes with layout */}
         <Route
-          index
-          element={<Home />}
-        />
+          path="/"
+          element={<Layout />}>
+          <Route
+            index
+            element={<Home />}
+          />
+          <Route
+            path="/student"
+            element={<Students />}
+          />
+          <Route
+            path="/score"
+            element={<Score />}
+          />
+          <Route
+            path="/analysis"
+            element={<Analysis />}
+          />
+          <Route
+            path="/preferences"
+            element={<Preferences />}
+          />
+          <Route
+            path="/support"
+            element={<Support />}
+          />
+          <Route
+            path="/profile"
+            element={<Profile />}
+          />
+        </Route>
+        {/* routes without layout */}
         <Route
-          path="/student"
-          element={<Students />}
+          path="/login"
+          element={<Login />}
         />
+        {/* not exist routes */}
         <Route
-          path="/score"
-          element={<Score />}
+          path="*"
+          element={<ErrorPage />}
         />
-        <Route
-          path="/analysis"
-          element={<Analysis />}
-        />
-        <Route
-          path="/preferences"
-          element={<Preferences />}
-        />
-        <Route
-          path="/support"
-          element={<Support />}
-        />
-        <Route
-          path="/profile"
-          element={<Profile />}
-        />
-      </Route>
-      {/* routes without layout */}
-      <Route
-        path="/login"
-        element={<Login />}
-      />
-      {/* not exist routes */}
-      <Route
-        path="*"
-        element={<ErrorPage />}
-      />
-    </Routes>
+      </Routes>
+    </>
   );
 }
 
