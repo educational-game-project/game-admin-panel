49ed647f835163d6aa94460041b076d46f59250f
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-11-05 18:26:23 +0700

    `update` assets

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,4 +1,5 @@
 import appleIcon from './apple-icon.png';
 import googleIcon from './google-icon.png';
+import desktopIcon from './desktop-icon.png';
 
-export { appleIcon, googleIcon };
+export { appleIcon, googleIcon, desktopIcon };
