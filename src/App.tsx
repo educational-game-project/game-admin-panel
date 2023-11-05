78e10023c6115239ebcadb83e23e2ace672ecf3b
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-11-05 16:34:09 +0700

    `update` app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { ToastContainer, Slide } from 'react-toastify';
 import Layout from './common/Layout';
 import Home from './pages/Home';
 import Students from './pages/Students';
@@ -9,57 +10,68 @@ import Support from './pages/Support';
 import Profile from './pages/Profile';
 import ErrorPage from './pages/Error';
 import Login from './pages/auth/Login';
-import AutoScrollToTop from './components/AutoScrollToTop';
+import AutoTopProvider from './components/AutoTopProvider';
+import ToastProvider from './components/ToastProvider';
+
+import 'react-toastify/dist/ReactToastify.min.css';
 
 function App() {
   return (
     <>
-      <AutoScrollToTop />
-      <Routes>
-        {/* routes with layout */}
-        <Route
-          path="/"
-          element={<Layout />}>
-          <Route
-            index
-            element={<Home />}
-          />
-          <Route
-            path="/student"
-            element={<Students />}
-          />
-          <Route
-            path="/score"
-            element={<Score />}
-          />
-          <Route
-            path="/analysis"
-            element={<Analysis />}
-          />
-          <Route
-            path="/preferences"
-            element={<Preferences />}
-          />
-          <Route
-            path="/support"
-            element={<Support />}
-          />
-          <Route
-            path="/profile"
-            element={<Profile />}
-          />
-        </Route>
-        {/* routes without layout */}
-        <Route
-          path="/login"
-          element={<Login />}
+      <AutoTopProvider />
+      <ToastProvider>
+        <ToastContainer
+          limit={3}
+          transition={Slide}
+          draggable={false}
+          closeOnClick={true}
         />
-        {/* not exist routes */}
-        <Route
-          path="*"
-          element={<ErrorPage />}
-        />
-      </Routes>
+        <Routes>
+          {/* routes with layout */}
+          <Route
+            path="/"
+            element={<Layout />}>
+            <Route
+              index
+              element={<Home />}
+            />
+            <Route
+              path="/student"
+              element={<Students />}
+            />
+            <Route
+              path="/score"
+              element={<Score />}
+            />
+            <Route
+              path="/analysis"
+              element={<Analysis />}
+            />
+            <Route
+              path="/preferences"
+              element={<Preferences />}
+            />
+            <Route
+              path="/support"
+              element={<Support />}
+            />
+            <Route
+              path="/profile"
+              element={<Profile />}
+            />
+          </Route>
+          {/* routes without layout */}
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          {/* not exist routes */}
+          <Route
+            path="*"
+            element={<ErrorPage />}
+          />
+        </Routes>
+      </ToastProvider>
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,26 @@
+import 'react-toastify/dist/ReactToastify.min.css';
+import {
+  showErrorToast,
+  showInfoToast,
+  showSuccessToast,
+  showWarningToast,
+} from '../utilities/toastUtils';
+
 function Home() {
+  const notify_1 = () => showSuccessToast('Login successfully !');
+  const notify_2 = () => showInfoToast('Login successfully !');
+  const notify_3 = () => showWarningToast('Login successfully !');
+  const notify_4 = () => showErrorToast('Login successfully !');
+
   return (
     <div className="h-[1500px]">
       <h5 className="font-bold text-xl">Home</h5>
+      <div>
+        <button onClick={notify_1}>Notify 1 !</button>
+        <button onClick={notify_2}>Notify 2 !</button>
+        <button onClick={notify_3}>Notify 3 !</button>
+        <button onClick={notify_4}>Notify 4 !</button>
+      </div>
     </div>
   );
 }
