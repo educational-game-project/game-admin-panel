d767feba20cecfdc0d360e5331a4188064a4c5f7
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-11-05 18:25:04 +0700

    `setup` clipboard-polyfill

---
+++ src/components/ButtonClipboard.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react';
+import { showDefaultToast, showErrorToast } from '../utilities/toastUtils';
+
+type ButtonClipboardProps = {
+  linkToCopy: string;
+  children: ReactNode;
+};
+
+const ButtonClipboard = ({ linkToCopy, children }: ButtonClipboardProps) => {
+  const handleCopyClick = () => {
+    try {
+      navigator.clipboard.writeText(linkToCopy);
+      showDefaultToast('Link copied to clipboard');
+    } catch (error) {
+      console.error('Failed to copy link: ', error);
+      showErrorToast('Failed to copy link');
+    }
+  };
+
+  return (
+    <button
+      onClick={handleCopyClick}
+      className="w-full bg-violet-500 rounded-md px-6 py-4.5 text-base font-semibold text-gray-200 transition-all-200 hover:bg-indigo-500 hover:text-gray-50">
+      {children}
+    </button>
+  );
+};
+
+export default ButtonClipboard;
