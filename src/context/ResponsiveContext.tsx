d0c4d6500f63d81ec120d9e72b686e4929c360fe
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-11-05 18:26:41 +0700

    `create` responsive context

---
+++ src/context/ResponsiveContext.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useEffect, useState, useContext } from 'react';
+
+type ResponsiveProviderProps = {
+  children: React.ReactNode;
+};
+const ResponsiveContext = createContext<boolean | undefined>(undefined);
+
+export const ResponsiveProvider = ({ children }: ResponsiveProviderProps) => {
+  const [isDesktopView, setIsDesktopView] = useState<boolean>(
+    window.innerWidth > 768
+  );
+
+  const handleResize = () => {
+    setIsDesktopView(window.innerWidth > 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <ResponsiveContext.Provider value={isDesktopView}>
+      {children}
+    </ResponsiveContext.Provider>
+  );
+};
+
+export function useResponsiveLayout() {
+  const context = useContext(ResponsiveContext);
+  if (context === undefined) {
+    throw new Error(
+      'useResponsiveLayout must be used within a ResponsiveProvider'
+    );
+  }
+  return context;
+}
