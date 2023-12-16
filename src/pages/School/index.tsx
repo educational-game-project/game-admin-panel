414143549f7c775717beba6cfa6a59ef73f82c21
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-12-16 20:24:04 +0700

    `create` school features

--- src/pages/School/index.tsx
+++ src/pages/School/index.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import Breadcrumbs from '../../components/Breadcrumbs';
 import { useBreadcrumbs } from '../../context/BreadcrumbsContext';
-import { SchoolIcon } from 'lucide-react';
+import { PlusIcon, SchoolIcon } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import SchoolTable from '../../components/Table/SchoolTable';
 
 function School() {
   const { setBreadcrumbs } = useBreadcrumbs();
@@ -24,10 +26,31 @@ function School() {
     <div className="">
       <div className="mb-6">
         <Breadcrumbs />
-        <h5 className="font-semibold text-3xl mb-1.5">Sekolah</h5>
-        <p className="text-gray-500">Lihat daftar sekolah yang terdaftar.</p>
+        <div className="flex justify-between">
+          <div className="">
+            <h5 className="font-semibold text-3xl mb-1.5">Sekolah</h5>
+            <p className="text-gray-500">
+              Lihat daftar sekolah yang terdaftar.
+            </p>
+          </div>
+          <div className="">
+            <Link
+              type="button"
+              className="leading-normal ml-4 inline-flex justify-center rounded-lg border border-transparent bg-violet-600 px-4 py-2.5 text-sm font-medium text-gray-100 hover:bg-violet-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-500 focus-visible:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-violet-500 disabled:focus-visible:ring-2 disabled:focus-visible:ring-violet-500 disabled:focus-visible:ring-offset-2"
+              to="/school/add">
+              <PlusIcon
+                size={20}
+                className="mr-1.5 stroke-current"
+                strokeWidth={2}
+              />
+              Tambah Sekolah
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="h-[1500px] bg-white p-5 rounded-xl"></div>
+      <div className="bg-white p-5 rounded-xl">
+        <SchoolTable />
+      </div>
     </div>
   );
 }
