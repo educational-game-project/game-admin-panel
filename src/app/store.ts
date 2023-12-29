765e89a74532bbb5286f98b51c5a76678746de7f
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-12-30 02:12:32 +0700

    `chore` redux toolkit RTK Query

---
+++ src/app/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authSlice from '../pages/Auth/features/authSlice';
+import { authApi } from '../services/auth';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
+export const store = configureStore({
+  reducer: {
+    auth: authSlice,
+    [authApi.reducerPath]: authApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
+});
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { SidebarProvider } from "./context/SidebarContext.tsx";
-import { ResponsiveProvider } from "./context/ResponsiveContext.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import { SidebarProvider } from './context/SidebarContext.tsx';
+import { ResponsiveProvider } from './context/ResponsiveContext.tsx';
+import { store } from './app/store.ts';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ResponsiveProvider>
-        <SidebarProvider>
-          <App />
-        </SidebarProvider>
-      </ResponsiveProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ResponsiveProvider>
+          <SidebarProvider>
+            <App />
+          </SidebarProvider>
+        </ResponsiveProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
