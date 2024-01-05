07456f1d32f4c99ddf4534e782f5b938d90f9c6b
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2024-01-05 15:16:47 +0700

    `style` update assets of image

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,5 +1,13 @@
 import appleIcon from './apple-icon.png';
 import googleIcon from './google-icon.png';
 import desktopIcon from './desktop-icon.png';
+import bgError from './question_bg.png';
 
-export { appleIcon, googleIcon, desktopIcon };
+const images = {
+  appleIcon,
+  googleIcon,
+  desktopIcon,
+  bgError,
+};
+
+export default images;
