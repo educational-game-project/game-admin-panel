57acffbccc0c65f11ea75425f2703710f46d414b
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2024-01-13 16:07:20 +0700

    `feat(auth)` update login page

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,34 +1,23 @@
-import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { CheckIcon, Eye, EyeOff } from "lucide-react";
-import { useLoginMutation } from "../../services/auth";
-import { setAuth } from "./features/authSlice";
-import { showDefaultToast, showErrorToast } from "../../components/Toast";
-// import axios from 'axios';
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { CheckIcon, Eye, EyeOff } from 'lucide-react';
+import { useLoginMutation } from '../../services/auth';
+import { setAuth } from './features/authSlice';
+import { showDefaultToast, showErrorToast } from '../../components/Toast';
 
-interface LoginFormProps {
-  email: string;
-  password: string;
-  remember?: boolean;
-}
-
-// const BASE_URL =
-//   import.meta.env.VITE_MODE === 'development'
-//     ? import.meta.env.VITE_API_DEV_URL
-//     : import.meta.env.VITE_API_URL;
+import { LoginFormProps } from './type';
 
 const schema = yup.object().shape({
-  email: yup.string().required("Email harus diisi").email("Email tidak valid"),
-  password: yup.string().required("Password harus diisi"),
+  email: yup.string().required('Email harus diisi').email('Email tidak valid'),
+  password: yup.string().required('Password harus diisi'),
 });
 
 function Login() {
   const [showPassword, setShowPassword] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [login] = useLoginMutation();
+  const [login, { isLoading }] = useLoginMutation();
 
   const {
     register,
@@ -37,42 +26,36 @@ function Login() {
     setValue,
     watch,
   } = useForm<LoginFormProps>({
-    mode: "onTouched",
+    mode: 'onTouched',
     resolver: yupResolver(schema),
   });
 
-  const rememberVal = watch("remember", false);
+  const rememberVal = watch('remember', false);
 
   const onLogin: SubmitHandler<LoginFormProps> = async (data) => {
-    setIsLoading(true);
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-    // console.log(data);
-
-    // try {
-    //   const result = await axios.post(`${BASE_URL}/auth/login`, {
-    //     email: data.email,
-    //     password: data.password,
-    //   });
-    //   showDefaultToast('Login success!');
-    //   console.log(result.data);
-    // } catch (error) {
-    //   console.log(error);
-    //   showErrorToast('Login failed!');
-    // }
-
     try {
-      console.log(data);
       const result = await login({
         email: data.email,
         password: data.password,
       }).unwrap();
+      console.log(
+        '🚀 ~ constonLogin:SubmitHandler<LoginFormProps>= ~ result:',
+        result
+      );
       setAuth(result.data);
-      showDefaultToast("Login success!");
-    } catch (error) {
-      console.error("Login failed:", error);
-      showErrorToast("Login failed!");
+      showDefaultToast('Login sukses!');
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.log(
+        '🚀 ~ constonLogin:SubmitHandler<LoginFormProps>= ~ error:',
+        error
+      );
+      if (error.data.message) {
+        showErrorToast(error.data.message);
+      } else {
+        showErrorToast('Login gagal!');
+      }
     }
-    setIsLoading(false);
   };
 
   return (
@@ -104,13 +87,13 @@ function Login() {
                     type="email"
                     className={`p-4 rounded-xl border bg-gray-50 border-gray-300 w-full focus:bg-white focus:outline focus:outline-4 focus:outline-offset-0 focus:outline-indigo-500/30 focus:border-indigo-500/80 ${
                       errors.email
-                        ? "bg-red-50 border-red-400 focus:outline-red-500/30 focus:border-red-500"
-                        : ""
+                        ? 'bg-red-50 border-red-400 focus:outline-red-500/30 focus:border-red-500'
+                        : ''
                     }`}
                     placeholder="Masukkan email Anda"
                     aria-required="true"
-                    aria-invalid={errors.email ? "true" : "false"}
-                    {...register("email")}
+                    aria-invalid={errors.email ? 'true' : 'false'}
+                    {...register('email')}
                   />
                   {errors.email && (
                     <p className="mt-1 -mb-1.5 text-red-500">
@@ -122,16 +105,16 @@ function Login() {
                   <div className="relative">
                     <input
                       id="password"
-                      type={showPassword ? "text" : "password"}
+                      type={showPassword ? 'text' : 'password'}
                       className={`p-4 rounded-xl border bg-gray-50 border-gray-300 w-full focus:bg-white focus:outline focus:outline-4 focus:outline-offset-0 focus:outline-indigo-500/30 focus:border-indigo-500/80 ${
                         errors.password
-                          ? "bg-red-50 border-red-400 focus:outline-red-500/30 focus:border-red-500"
-                          : ""
+                          ? 'bg-red-50 border-red-400 focus:outline-red-500/30 focus:border-red-500'
+                          : ''
                       }`}
                       placeholder="Masukkan password Anda"
                       aria-required="true"
-                      aria-invalid={errors.password ? "true" : "false"}
-                      {...register("password")}
+                      aria-invalid={errors.password ? 'true' : 'false'}
+                      {...register('password')}
                     />
                     {showPassword ? (
                       <Eye
@@ -160,18 +143,17 @@ function Login() {
                         type="checkbox"
                         id="remember"
                         className="w-4.5 h-4.5 opacity-0 invisible -z-10"
-                        {...register("remember")}
+                        {...register('remember')}
                       />
                       <div className="absolute left-0 top-px">
                         <div
                           className={`w-4.5 h-4.5 rounded-full flex items-center justify-center ${
-                            rememberVal ? "bg-indigo-500" : "bg-gray-300"
+                            rememberVal ? 'bg-indigo-500' : 'bg-gray-300'
                           }`}
                           role="checkbox"
                           aria-checked="false"
                           title="Remember me"
-                          onClick={() => setValue("remember", !rememberVal)}
-                        >
+                          onClick={() => setValue('remember', !rememberVal)}>
                           <CheckIcon
                             size={10}
                             strokeWidth={4}
@@ -180,14 +162,15 @@ function Login() {
                         </div>
                       </div>
                     </div>
-                    <label htmlFor="remember" className="ml-2.5">
+                    <label
+                      htmlFor="remember"
+                      className="ml-2.5">
                       Ingat saya
                     </label>
                   </div>
                   <Link
                     to="/fogot-password"
-                    className="text-sky-500 hover:text-indigo-500 hover:underline underline-offset-2"
-                  >
+                    className="text-sky-500 hover:text-indigo-500 hover:underline underline-offset-2">
                     Lupa Password?
                   </Link>
                 </div>
@@ -195,28 +178,24 @@ function Login() {
                   title="Signin"
                   type="submit"
                   className="p-4 rounded-xl text-base w-full text-center bg-indigo-600 transition-all-200 text-white font-semibold hover:bg-indigo-700 flex items-center justify-center disabled:bg-indigo-300 disabled:text-white/70 disabled:cursor-not-allowed"
-                  disabled={isLoading}
-                >
+                  disabled={isLoading}>
                   {isLoading && (
                     <svg
                       className="animate-spin-fast -ml-1 mr-3 h-5 w-5 text-white inline-block"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
-                      viewBox="0 0 24 24"
-                    >
+                      viewBox="0 0 24 24">
                       <circle
                         className="opacity-25"
                         cx="12"
                         cy="12"
                         r="10"
                         stroke="currentColor"
-                        strokeWidth="4"
-                      ></circle>
+                        strokeWidth="4"></circle>
                       <path
                         className="opacity-75"
                         fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      ></path>
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                     </svg>
                   )}
                   Masuk
@@ -226,8 +205,7 @@ function Login() {
             <div className="flex items-center justify-between mx-10 mb-8">
               <Link
                 to="/privacy"
-                className="text-gray-400 hover:underline underline-offset-2"
-              >
+                className="text-gray-400 hover:underline underline-offset-2">
                 Privacy Policy
               </Link>
               <p className="text-gray-400">Copyright 2023</p>
