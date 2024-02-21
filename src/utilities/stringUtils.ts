3a6571dd30653ae65230c8640d9282b5933a0ff6
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2024-02-21 20:17:03 +0700

    `chore` update text utils

--- src/utilities/stringUtils.ts
+++ src/utilities/stringUtils.ts
@@ -4,3 +4,32 @@ export const transformStringPlus = (input: string | undefined): string => {
   }
   return input.replace(/\s+/g, '+');
 };
+
+export const extractNameParts = (fullName: string | undefined) => {
+  if (fullName === undefined) {
+    return {
+      firstName: '',
+      lastName: '',
+    };
+  }
+
+  const nameParts = fullName.split(' ');
+  if (nameParts.length === 2) {
+    return {
+      firstName: nameParts[0],
+      lastName: nameParts[1],
+    };
+  } else if (nameParts.length > 2) {
+    const firstName = nameParts.slice(0, -1).join(' ');
+    const lastName = nameParts[nameParts.length - 1];
+    return {
+      firstName: firstName,
+      lastName: lastName,
+    };
+  } else {
+    return {
+      firstName: fullName,
+      lastName: '',
+    };
+  }
+};
