b7cccc0917d5832ca28aae7f3e83d8c06f94aeb9
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2024-02-24 23:24:53 +0700

    `feat(admi)` update admin api

--- src/services/adminApi.ts
+++ src/services/adminApi.ts
@@ -3,6 +3,7 @@ import { coreApi } from '../api/coreApi';
 import type {
   AdminIdRequest,
   AdminListSuccessResponse,
+  AdminSuccessResponse,
   DataTableGetRequest,
   SuccessResponse,
 } from '../types';
@@ -16,6 +17,52 @@ export const adminApi = coreApi.injectEndpoints({
         body: data,
       }),
     }),
+    getAdminById: builder.mutation<AdminSuccessResponse, AdminIdRequest>({
+      query: (id) => ({
+        url: '/user/admin/detail',
+        method: 'POST',
+        body: id,
+      }),
+    }),
+    addAdmin: builder.mutation({
+      query: (data) => {
+        const formAddAdmin = new FormData();
+        formAddAdmin.append('name', data.name);
+        formAddAdmin.append('email', data.email);
+        formAddAdmin.append('phoneNumber', data.phoneNumber);
+        formAddAdmin.append('password', data.password);
+        formAddAdmin.append('schoolId', data.school);
+        if (data?.media) {
+          formAddAdmin.append('media', data?.media[0] || '');
+        }
+        return {
+          url: '/user/admin',
+          method: 'POST',
+          body: formAddAdmin,
+          formData: true,
+        };
+      },
+    }),
+    updateAdmin: builder.mutation({
+      query: (data) => {
+        const formEditAdmin = new FormData();
+        formEditAdmin.append('id', data.id);
+        formEditAdmin.append('name', data.name);
+        formEditAdmin.append('email', data.email);
+        formEditAdmin.append('phoneNumber', data.phoneNumber);
+        formEditAdmin.append('schoolId', data.school);
+        if (data?.media) {
+          formEditAdmin.append('media', data?.media[0] || '');
+        }
+        return {
+          url: '/user/admin',
+          method: 'PUT',
+          body: formEditAdmin,
+          formData: true,
+        };
+      },
+      invalidatesTags: ['Student'],
+    }),
     deleteAdmin: builder.mutation<SuccessResponse, AdminIdRequest>({
       query: (id) => ({
         url: `/user/admin`,
@@ -26,4 +73,10 @@ export const adminApi = coreApi.injectEndpoints({
   }),
 });
 
-export const { useDeleteAdminMutation, useGetAdminMutation } = adminApi;
+export const {
+  useAddAdminMutation,
+  useDeleteAdminMutation,
+  useGetAdminByIdMutation,
+  useGetAdminMutation,
+  useUpdateAdminMutation,
+} = adminApi;
