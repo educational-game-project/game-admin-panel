93b45b3ec8ae413151a1a287ab008b2c009e7dbb
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2024-02-26 08:55:11 +0700

    `feat(game)` create game api service

---
+++ src/services/gameApi.ts
@@ -0,0 +1,82 @@
+import { coreApi } from '../api/coreApi';
+
+import type {
+  DataTableGetRequest,
+  GameIdRequest,
+  GameListSuccessResponse,
+  GameSuccessResponse,
+  SuccessResponse,
+} from '../types';
+
+export const gameApi = coreApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getGame: builder.mutation<GameListSuccessResponse, DataTableGetRequest>({
+      query: (data) => ({
+        url: '/games/find',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    getGameById: builder.mutation<GameSuccessResponse, GameIdRequest>({
+      query: (id) => ({
+        url: '/games/detail',
+        method: 'POST',
+        body: id,
+      }),
+    }),
+    defineGame: builder.mutation({
+      query: (data) => {
+        const formDefineGame = new FormData();
+        formDefineGame.append('name', data.name);
+        formDefineGame.append('author', data.author);
+        formDefineGame.append('description', data.description);
+        formDefineGame.append('category', data.category);
+        formDefineGame.append('maxLevel', data.maxLevel);
+        formDefineGame.append('maxRetry', data.maxRetry);
+        formDefineGame.append('maxTime', data.maxTime);
+        formDefineGame.append('media', data?.media[0]);
+        return {
+          url: '/games',
+          method: 'POST',
+          body: formDefineGame,
+          formData: true,
+        };
+      },
+    }),
+    updateGame: builder.mutation({
+      query: (data) => {
+        const formEditGame = new FormData();
+        formEditGame.append('id', data.id);
+        formEditGame.append('name', data.name);
+        formEditGame.append('author', data.author);
+        formEditGame.append('description', data.description);
+        formEditGame.append('category', data.category);
+        formEditGame.append('maxLevel', data.maxLevel);
+        formEditGame.append('maxRetry', data.maxRetry);
+        formEditGame.append('maxTime', data.maxTime);
+        formEditGame.append('media', data?.media[0]);
+        return {
+          url: '/games',
+          method: 'PUT',
+          body: formEditGame,
+          formData: true,
+        };
+      },
+    }),
+    deleteGame: builder.mutation<SuccessResponse, GameIdRequest>({
+      query: (id) => ({
+        url: '/games',
+        method: 'DELETE',
+        body: id,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useDefineGameMutation,
+  useDeleteGameMutation,
+  useGetGameByIdMutation,
+  useGetGameMutation,
+  useUpdateGameMutation,
+} = gameApi;
