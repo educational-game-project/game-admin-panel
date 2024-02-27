bfd98eafe0d4ee3c9a04a0c0f4e74e53546c411e
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2024-02-27 12:03:59 +0700

    `chore` update types

--- src/types/apiType.ts
+++ src/types/apiType.ts
@@ -224,93 +224,6 @@ interface LeaderboardSuccessResponse extends SuccessResponse {
   data: LeaderboardResponse[];
 }
 
-// CHECK DULU ========================================
-interface BaseImageProps {
-  originalname: string;
-  filename: string;
-  fileLink: string;
-  mimeType: string;
-  isDefault: boolean;
-  deletedAt: string | null;
-  _id: string;
-  createdAt: string;
-  updatedAt: string;
-  size?: number;
-}
-interface BaseSchoolAdminProps {
-  _id: string;
-  name: string;
-  address: string;
-  images: string | BaseImageProps[];
-  adminsCount: number;
-  studentsCount: number;
-  deletedAt: string | null;
-  createdAt: string;
-  updatedAt: string;
-  createdAtString: string;
-  updatedAtString: string;
-}
-
-interface ScoreProps {
-  id: string;
-  name: string;
-  score: number;
-}
-interface StudentProps {
-  _id: string;
-  name: string;
-  role: string;
-  image: BaseImageProps;
-  email: string;
-  phoneNumber: string;
-  password: string;
-  deletedAt: string;
-  school: BaseSchoolAdminProps;
-  createdAt: string;
-  createdAtString: string;
-  updatedAtString: string;
-}
-interface AdminProps {
-  _id: string;
-  name: string;
-  role: string;
-  images: BaseImageProps[];
-  email: string;
-  phoneNumber: string;
-  password: string;
-  deletedAt: string | null;
-  school: BaseSchoolAdminProps;
-  createdAt: string;
-  updatedAt: string;
-  createdAtString: string;
-  updatedAtString: string;
-}
-interface ScoreResponse {
-  data: ScoreProps[];
-}
-interface StudentResponse {
-  data: StudentProps[];
-}
-interface AdminResponse {
-  data: AdminProps[];
-}
-interface StudentProps {
-  _id: string;
-  name: string;
-  role: string;
-  images: BaseImageProps;
-  email: string;
-  phoneNumber: string;
-  password: string;
-  deletedAt: string;
-  school: BaseSchoolAdminProps;
-  createdAt: string;
-  createdAtString: string;
-  updatedAtString: string;
-}
-
-// ====================
-
 export type {
   Admin,
   AdminListSuccessResponse,
@@ -332,11 +245,4 @@ export type {
   StudentSuccessResponse,
   SuccessResponse,
   User,
-  // CHECK DULU =================
-  ScoreProps,
-  ScoreResponse,
-  StudentProps,
-  StudentResponse,
-  AdminProps,
-  AdminResponse,
 };
