3d544f1c576463398a71ba35632e51cab129373e
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2024-03-01 23:48:09 +0700

    `feat(dashboard)` create dashboard api

---
+++ src/services/dashboardApi.ts
@@ -0,0 +1,13 @@
+import { coreApi } from '../api/coreApi';
+import type { DashboardSuccessResponse } from '../types';
+
+export const dashboardApi = coreApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getDashboard: builder.query<DashboardSuccessResponse, void>({
+      query: () => '/dashboard',
+      providesTags: ['Dashboard'],
+    }),
+  }),
+});
+
+export const { useGetDashboardQuery } = dashboardApi;
