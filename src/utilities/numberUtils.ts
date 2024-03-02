8953ae99fb47e24b51bd1653abc81964d14c47bc
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2024-03-02 13:42:04 +0700

    `chore` update app config

--- src/utilities/numberUtils.ts
+++ src/utilities/numberUtils.ts
@@ -4,3 +4,24 @@ export const transformInteger = (input: number | undefined): number => {
   }
   return Math.round(input);
 };
+
+export const transformPercentage = (input: number | undefined): string => {
+  if (input === undefined) {
+    return '0%';
+  }
+  return `${input.toFixed(1)}%`;
+};
+
+export const getPercentage = (
+  input: number | undefined,
+  total: number | undefined
+): string => {
+  if (input === undefined || total === undefined) {
+    return '0';
+  }
+  if (total === 0) {
+    return '0';
+  }
+  const percentValue = (input / total) * 100;
+  return transformPercentage(percentValue);
+};
