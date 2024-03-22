c69362334ed10c9ab4dad004a7fda0bf2cf12fcb
Khoeru Roziqin <roziqinkhoeru8@gmail.com>
2024-03-22 23:40:26 +0700

    `styles` update image config

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,17 +1,17 @@
-import appleIcon from './apple-icon.png';
-import googleIcon from './google-icon.png';
-import desktopIcon from './desktop-icon.png';
-import bgError from './question_bg.png';
-import bgErrorDark from './question_bg_dark.png';
-import dominoBluePurpleImage from './domino-blue-purple.jpg';
+import appleIcon from "./apple-icon.png";
+import googleIcon from "./google-icon.png";
+import desktopIcon from "./desktop-icon.png";
+import bgError from "./question_bg.png";
+import bgErrorDark from "./question_bg_dark.png";
+import dominoBluePurpleImage from "./domino-blue-purple.jpg";
 
 const images = {
-  appleIcon,
-  dominoBluePurpleImage,
-  googleIcon,
-  desktopIcon,
-  bgError,
-  bgErrorDark,
+	appleIcon,
+	dominoBluePurpleImage,
+	googleIcon,
+	desktopIcon,
+	bgError,
+	bgErrorDark,
 };
 
 export default images;
