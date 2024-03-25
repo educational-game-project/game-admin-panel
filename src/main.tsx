383fa67415453c739ba4bae71c6d8962d646a603
Khoeru Roziqin <roziqinkhoeru8@gmail.com>
2024-03-25 12:51:58 +0700

    `chore` update main app

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import { store } from "./app/store.ts";
-
 import { ResponsiveProvider } from "./context/ResponsiveContext.tsx";
+import { store } from "./app/store.ts";
 
 import "./index.css";
 
