7e9c2f77bd5d42896ab915090336e529f77155cb
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-07-04 00:08:04 +0700

    Fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import { ToastContainer, Slide } from "react-toastify";
 import { type Action, KBarProvider } from "kbar";
 import {
-	ActivityIcon,
-	AppWindowIcon,
-	Gamepad2Icon,
-	HomeIcon,
-	Share2Icon,
-	UserSquare2Icon,
+  ActivityIcon,
+  AppWindowIcon,
+  Gamepad2Icon,
+  HomeIcon,
+  Share2Icon,
+  UserSquare2Icon,
 } from "lucide-react";
 import { useUser } from "./hook/authHooks";
 import { useResponsiveLayout } from "./hook/responsiveHooks";
@@ -50,196 +50,197 @@ import images from "./assets/img";
 import "react-toastify/dist/ReactToastify.min.css";
 
 function App() {
-	const isDesktopView = useResponsiveLayout();
-	const navigate = useNavigate();
-	const { user } = useUser();
+  const isDesktopView = useResponsiveLayout();
+  const navigate = useNavigate();
+  const { user } = useUser();
 
-	const onClickLink = (url: string) => navigate(url);
+  const onClickLink = (url: string) => navigate(url);
 
-	const searchActions: Action[] = [
-		{
-			id: "home",
-			name: "Home",
-			keywords: "home dashboard root",
-			shortcut: ["h"],
-			perform: () => onClickLink("/"),
-			section: "Pages",
-			icon: <HomeIcon size={18} />,
-		},
-		{
-			id: "game",
-			name: "Game",
-			shortcut: ["g"],
-			keywords: "game permainan",
-			perform: () => onClickLink("/game"),
-			section: "Pages",
-			icon: <Gamepad2Icon size={18} />,
-		},
-		{
-			id: "student",
-			name: "Siswa",
-			shortcut: ["t"],
-			keywords: "siswa student user",
-			perform: () => onClickLink("/student"),
-			section: "Pages",
-			icon: <UserSquare2Icon size={18} />,
-		},
-		{
-			id: "activity",
-			name: "Aktivitas",
-			shortcut: ["a"],
-			keywords: "aktivitas activity",
-			perform: () => onClickLink("/activity"),
-			section: "Pages",
-			icon: <ActivityIcon size={18} />,
-		},
-		{
-			id: "features",
-			name: "Features",
-			keywords: "features fitur",
-			section: "Pages",
-			icon: <AppWindowIcon size={18} />,
-		},
-	];
+  const searchActions: Action[] = [
+    {
+      id: "home",
+      name: "Home",
+      keywords: "home dashboard root",
+      shortcut: ["h"],
+      perform: () => onClickLink("/"),
+      section: "Pages",
+      icon: <HomeIcon size={18} />,
+    },
+    {
+      id: "game",
+      name: "Game",
+      shortcut: ["g"],
+      keywords: "game permainan",
+      perform: () => onClickLink("/game"),
+      section: "Pages",
+      icon: <Gamepad2Icon size={18} />,
+    },
+    {
+      id: "student",
+      name: "Siswa",
+      shortcut: ["t"],
+      keywords: "siswa student user",
+      perform: () => onClickLink("/student"),
+      section: "Pages",
+      icon: <UserSquare2Icon size={18} />,
+    },
+    {
+      id: "activity",
+      name: "Aktivitas",
+      shortcut: ["a"],
+      keywords: "aktivitas activity",
+      perform: () => onClickLink("/activity"),
+      section: "Pages",
+      icon: <ActivityIcon size={18} />,
+    },
+    {
+      id: "features",
+      name: "Features",
+      keywords: "features fitur",
+      section: "Pages",
+      icon: <AppWindowIcon size={18} />,
+    },
+  ];
 
-	socials.map((social) => {
-		searchActions.push({
-			id: social.id,
-			name: social.name,
-			perform: () => window.open(social.url),
-			section: "Socials",
-			icon: social.icon,
-		});
-	});
+  socials.map((social) => {
+    searchActions.push({
+      id: social.id,
+      name: social.name,
+      perform: () => window.open(social.url),
+      section: "Socials",
+      icon: social.icon,
+    });
+  });
 
-	featuresData.features
-		.filter(
-			(feature) =>
-				user?.role === "Super Admin" || feature.role !== "Super Admin"
-		)
-		.map((feature) => {
-			searchActions.push({
-				id: feature.id,
-				name: feature.title,
-				perform: () => onClickLink(`/${feature.slug}`),
-				parent: "features",
-				keywords: feature.keywords,
-			});
-		});
+  featuresData.features
+    .filter(
+      (feature) =>
+        user?.role === "Super Admin" || feature.role !== "Super Admin"
+    )
+    .map((feature) => {
+      searchActions.push({
+        id: feature.id,
+        name: feature.title,
+        perform: () => onClickLink(`/${feature.slug}`),
+        parent: "features",
+        keywords: feature.keywords,
+      });
+    });
 
-	return (
-		<>
-			{isDesktopView ? (
-				<>
-					<AutoTopProvider />
-					<KBarProvider actions={searchActions}>
-						<GlobalSearch />
-						<ToastProvider>
-							<ToastContainer
-								limit={3}
-								transition={Slide}
-								draggable={false}
-								closeOnClick={true}
-							/>
-							<Routes>
-								{/* routes with layout */}
-								<Route path="/" element={<Layout />}>
-									<Route element={<ProtectedMiddleware />}>
-										<Route index element={<Home />} />
-										<Route element={<SecureGuardMiddleware />}>
-											<Route path="/admin" element={<Admin />} />
-											<Route path="/admin/add" element={<AddAdmin />} />
-											<Route
-												path="/admin/edit/:adminId"
-												element={<EditAdmin />}
-											/>
-											<Route path="/game/add" element={<AddGame />} />
-											<Route path="/game/edit/:gameId" element={<EditGame />} />
-											<Route path="/school" element={<School />} />
-											<Route path="/school/add" element={<AddSchool />} />
-											<Route
-												path="/school/edit/:schoolId"
-												element={<EditSchool />}
-											/>
-											<Route
-												path="/school/:schoolId"
-												element={<DetailSchool />}
-											/>
-										</Route>
-										<Route path="/game" element={<Game />} />
-										<Route path="/game/:gameId" element={<DetailGame />} />
-										<Route
-											path="/game/leaderboard/:gameId"
-											element={<Leaderboard />}
-										/>
-										<Route path="/student" element={<Students />} />
-										<Route path="/student/add" element={<AddStudent />} />
-										<Route
-											path="/student/edit/:studentId"
-											element={<EditStudent />}
-										/>
-										<Route
-											path="/student/score/:studentId"
-											element={<Score />}
-										/>
-										<Route path="/profile/account" element={<Profile />} />
-										<Route path="/activity" element={<Activity />} />
-										<Route
-											path="/profile/change-password"
-											element={<ChangePassword />}
-										/>
-									</Route>
-								</Route>
-								{/* routes without layout */}
-								<Route element={<AuthMiddleware />}>
-									<Route path="/login" element={<Login />} />
-								</Route>
-								{/* not exist routes */}
-								<Route path="*" element={<Navigate to="/404" replace />} />
-								<Route path="/404" element={<ErrorPage />} />
-							</Routes>
-						</ToastProvider>
-					</KBarProvider>
-				</>
-			) : (
-				<ToastProvider>
-					<ToastContainer
-						limit={3}
-						transition={Slide}
-						draggable={false}
-						closeOnClick={true}
-					/>
-					<main className="w-full min-h-screen bg-gradient-to-br from-indigo-400 from-20% via-sky-400 via-40% to-emerald-400 to-90% px-6 dark:from-indigo-600 dark:via-sky-600 dark:to-emerald-600">
-						<div className="h-full w-full min-h-screen flex justify-center items-center">
-							<div className="bg-white p-6 sm:p-8 rounded-xl w-auto sm:w-96 dark:bg-gray-800">
-								<p className="mb-4">
-									<img
-										src={images.desktopIcon}
-										alt="desktop icon"
-										className="h-14"
-									/>
-								</p>
-								<h4 className="mb-20 sm:mb-24 font-bold text-2xl text-gray-700 dark:text-gray-200">
-									Gameon is only available on desktop for now.
-								</h4>
-								<p className="text-center mb-3 text-gray-400">
-									Open this link on your desktop to access
-								</p>
-								<ButtonClipboard linkToCopy={window.location.href}>
-									<Share2Icon
-										size={20}
-										className="inline-block mr-2.5"
-										strokeWidth={3}
-									/>
-									<span>Share</span>
-								</ButtonClipboard>
-							</div>
-						</div>
-					</main>
-				</ToastProvider>
-			)}
-		</>
-	);
+  return (
+    <>
+      {isDesktopView ? (
+        <>
+          <AutoTopProvider />
+          <KBarProvider actions={searchActions}>
+            <GlobalSearch />
+            <ToastProvider>
+              <ToastContainer
+                limit={3}
+                transition={Slide}
+                draggable={false}
+                closeOnClick={true}
+              />
+              <Routes>
+                {/* routes with layout */}
+                <Route path="/" element={<Layout />}>
+                  <Route element={<ProtectedMiddleware />}>
+                    <Route index element={<Home />} />
+                    <Route element={<SecureGuardMiddleware />}>
+                      <Route path="/admin" element={<Admin />} />
+                      <Route path="/admin/add" element={<AddAdmin />} />
+                      <Route
+                        path="/admin/edit/:adminId"
+                        element={<EditAdmin />}
+                      />
+                      <Route path="/game/add" element={<AddGame />} />
+                      <Route path="/game/edit/:gameId" element={<EditGame />} />
+                      <Route path="/school" element={<School />} />
+                      <Route path="/school/add" element={<AddSchool />} />
+                      <Route
+                        path="/school/edit/:schoolId"
+                        element={<EditSchool />}
+                      />
+                      <Route
+                        path="/school/:schoolId"
+                        element={<DetailSchool />}
+                      />
+                    </Route>
+                    <Route path="/game" element={<Game />} />
+                    <Route path="/game/:gameId" element={<DetailGame />} />
+                    <Route
+                      path="/game/leaderboard/:gameId"
+                      element={<Leaderboard />}
+                    />
+                    <Route path="/student" element={<Students />} />
+                    <Route path="/student/add" element={<AddStudent />} />
+                    <Route
+                      path="/student/edit/:studentId"
+                      element={<EditStudent />}
+                    />
+                    <Route
+                      path="/student/score/:studentId"
+                      element={<Score />}
+                    />
+                    <Route path="/profile/account" element={<Profile />} />
+                    <Route path="/activity" element={<Activity />} />
+                    <Route
+                      path="/profile/change-password"
+                      element={<ChangePassword />}
+                    />
+                  </Route>
+                </Route>
+                {/* routes without layout */}
+                <Route element={<AuthMiddleware />}>
+                  <Route path="/login" element={<Login />} />
+                </Route>
+                {/* not exist routes */}
+                <Route path="*" element={<Navigate to="/404" replace />} />
+                <Route path="/404" element={<ErrorPage />} />
+              </Routes>
+            </ToastProvider>
+          </KBarProvider>
+        </>
+      ) : (
+        <ToastProvider>
+          <ToastContainer
+            limit={3}
+            transition={Slide}
+            draggable={false}
+            closeOnClick={true}
+          />
+          <main className="w-full min-h-screen bg-gradient-to-br from-indigo-400 from-20% via-sky-400 via-40% to-emerald-400 to-90% px-6 dark:from-indigo-600 dark:via-sky-600 dark:to-emerald-600">
+            <div className="h-full w-full min-h-screen flex justify-center items-center">
+              <div className="bg-white p-6 sm:p-8 rounded-xl w-auto sm:w-96 dark:bg-gray-800">
+                <p className="mb-4">
+                  <img
+                    src={images.desktopIcon}
+                    alt="desktop icon"
+                    className="h-14"
+                  />
+                </p>
+                <h4 className="mb-20 sm:mb-24 font-bold text-2xl text-gray-700 dark:text-gray-200">
+                  Educational Game Admin Dashboard is only available on desktop
+                  for now.
+                </h4>
+                <p className="text-center mb-3 text-gray-400">
+                  Open this link on your desktop to access
+                </p>
+                <ButtonClipboard linkToCopy={window.location.href}>
+                  <Share2Icon
+                    size={20}
+                    className="inline-block mr-2.5"
+                    strokeWidth={3}
+                  />
+                  <span>Share</span>
+                </ButtonClipboard>
+              </div>
+            </div>
+          </main>
+        </ToastProvider>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,173 +14,175 @@ import type { School, SchoolChartType } from "../../types";
 import { AlertTriangleIcon } from "lucide-react";
 
 function Home() {
-	const dispatch = useAppDispatch();
-	const {
-		data: dashboards,
-		isLoading,
-		isError,
-		isSuccess,
-	} = useGetDashboardQuery();
+  const dispatch = useAppDispatch();
+  const {
+    data: dashboards,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useGetDashboardQuery();
 
-	if (isError) {
-		showErrorToast("Gagal memuat data");
-	}
+  if (isError) {
+    showErrorToast("Gagal memuat data");
+  }
 
-	const transformSchoolData = (
-		schoolsData: School[] | undefined
-	): SchoolChartType[] => {
-		if (!schoolsData || !Array.isArray(schoolsData)) {
-			return [];
-		}
-		return schoolsData.map((school) => ({
-			name: school.name,
-			admin: school.adminsCount,
-			student: school.studentsCount,
-		}));
-	};
+  const transformSchoolData = (
+    schoolsData: School[] | undefined
+  ): SchoolChartType[] => {
+    if (!schoolsData || !Array.isArray(schoolsData)) {
+      return [];
+    }
+    return schoolsData.map((school) => ({
+      name: school.name,
+      admin: school.adminsCount,
+      student: school.studentsCount,
+    }));
+  };
 
-	const schoolChart = useMemo(() => {
-		return transformSchoolData(dashboards?.data?.schools) || [];
-	}, [dashboards?.data?.schools]);
+  const schoolChart = useMemo(() => {
+    return transformSchoolData(dashboards?.data?.schools) || [];
+  }, [dashboards?.data?.schools]);
 
-	useEffect(() => {
-		dispatch(setBreadcrumb([]));
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(setBreadcrumb([]));
+  }, [dispatch]);
 
-	return (
-		<div>
-			<div className="mb-6">
-				<Breadcrumb />
-				<h5 className="font-semibold text-3xl mb-1.5">Home</h5>
-				<p className="text-gray-500">Selamat datang di Dashboard Gameon.</p>
-			</div>
-			<div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-6 gap-4">
-				<PerformanceWidget
-					countItem={dashboards?.data?.gameCount}
-					isError={isError}
-					name="permainan"
-					type="simple"
-				/>
-				<PerformanceWidget
-					countItem={dashboards?.data?.schoolCount}
-					isError={isError}
-					name="sekolah"
-					type="simple"
-				/>
-				<PerformanceWidget
-					activeItem={dashboards?.data?.activeAdmin}
-					countItem={dashboards?.data?.adminCount}
-					isError={isError}
-					name="admin"
-					type="advanced"
-				>
-					<PerformanceChart
-						baseColor="#f59e0b"
-						currentvalue={dashboards?.data?.activeAdmin}
-						totalValue={dashboards?.data?.adminCount}
-					/>
-				</PerformanceWidget>
-				<PerformanceWidget
-					activeItem={dashboards?.data?.activeStudents}
-					countItem={dashboards?.data?.studentsCount}
-					isError={isError}
-					name="siswa"
-					type="advanced"
-				>
-					<PerformanceChart
-						baseColor="#10b981"
-						currentvalue={dashboards?.data?.activeStudents}
-						totalValue={dashboards?.data?.studentsCount}
-					/>
-				</PerformanceWidget>
-			</div>
-			<div className="grid grid-cols-6 mb-6 gap-4">
-				<div className="col-span-full xl:col-span-4">
-					{isLoading && (
-						<div className="bg-white p-5 rounded-xl dark:bg-gray-800">
-							<div className="flex justify-between">
-								<div>
-									<div className="animate-pulse-fast skeleton-loader skeleton-sm w-48 mb-2" />
-									<div className="animate-pulse-fast skeleton-loader skeleton-sm w-72 mb-5" />
-								</div>
-								<div className="animate-pulse-fast skeleton-loader skeleton-sm w-16 h-8" />
-							</div>
-							<div className="w-full h-80 mb-1.5 flex items-center justify-center">
-								<BarsScaleFade
-									width="2rem"
-									height="2rem"
-									color="#6b7280"
-									dur={0.5}
-								/>
-							</div>
-						</div>
-					)}
-					{isSuccess && <SchoolOverview data={schoolChart} />}
-					{isError && (
-						<div className="bg-white p-5 rounded-xl dark:bg-gray-800">
-							<div className="mb-4">
-								<h5 className="font-semibold text-lg mb-0.5">
-									School Overview
-								</h5>
-								<p className="text-gray-500">
-									Overview of the school's performance.
-								</p>
-							</div>
-							<div className="w-full h-44 flex items-center justify-center flex-col">
-								<div className="mb-3">
-									<AlertTriangleIcon
-										size={42}
-										className="stroke-gray-500 dark:stroke-gray-400"
-									/>
-								</div>
-								<p className="text-center text-gray-500">
-									No data available. Please try again later.
-								</p>
-							</div>
-						</div>
-					)}
-				</div>
-				<div className="col-span-full xl:col-span-2">
-					{isLoading && (
-						<div className="bg-white p-5 rounded-xl dark:bg-gray-800">
-							<div className="animate-pulse-fast skeleton-loader skeleton-sm w-36 mb-2" />
-							<div className="animate-pulse-fast skeleton-loader skeleton-sm w-3/4 min-w-40 mb-5" />
-							<div className="w-full h-80 mb-1.5 flex items-center justify-center">
-								<BarsScaleFade
-									width="2rem"
-									height="2rem"
-									color="#6b7280"
-									dur={0.5}
-								/>
-							</div>
-						</div>
-					)}
-					{isSuccess && <GameOverview data={dashboards?.data?.games} />}
-					{isError && (
-						<div className="bg-white p-5 rounded-xl dark:bg-gray-800">
-							<div className="mb-4">
-								<h5 className="font-semibold text-lg mb-0.5">Game Overview</h5>
-								<p className="text-gray-500">
-									Overview of the game's performance.
-								</p>
-							</div>
-							<div className="w-full h-44 flex items-center justify-center flex-col">
-								<div className="mb-3">
-									<AlertTriangleIcon
-										size={42}
-										className="stroke-gray-500 dark:stroke-gray-400"
-									/>
-								</div>
-								<p className="text-center text-gray-500">
-									No data available. Please try again later.
-								</p>
-							</div>
-						</div>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="mb-6">
+        <Breadcrumb />
+        <h5 className="font-semibold text-3xl mb-1.5">Home</h5>
+        <p className="text-gray-500">
+          Selamat datang di Dashboard Educational Game.
+        </p>
+      </div>
+      <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-6 gap-4">
+        <PerformanceWidget
+          countItem={dashboards?.data?.gameCount}
+          isError={isError}
+          name="permainan"
+          type="simple"
+        />
+        <PerformanceWidget
+          countItem={dashboards?.data?.schoolCount}
+          isError={isError}
+          name="sekolah"
+          type="simple"
+        />
+        <PerformanceWidget
+          activeItem={dashboards?.data?.activeAdmin}
+          countItem={dashboards?.data?.adminCount}
+          isError={isError}
+          name="admin"
+          type="advanced"
+        >
+          <PerformanceChart
+            baseColor="#f59e0b"
+            currentvalue={dashboards?.data?.activeAdmin}
+            totalValue={dashboards?.data?.adminCount}
+          />
+        </PerformanceWidget>
+        <PerformanceWidget
+          activeItem={dashboards?.data?.activeStudents}
+          countItem={dashboards?.data?.studentsCount}
+          isError={isError}
+          name="siswa"
+          type="advanced"
+        >
+          <PerformanceChart
+            baseColor="#10b981"
+            currentvalue={dashboards?.data?.activeStudents}
+            totalValue={dashboards?.data?.studentsCount}
+          />
+        </PerformanceWidget>
+      </div>
+      <div className="grid grid-cols-6 mb-6 gap-4">
+        <div className="col-span-full xl:col-span-4">
+          {isLoading && (
+            <div className="bg-white p-5 rounded-xl dark:bg-gray-800">
+              <div className="flex justify-between">
+                <div>
+                  <div className="animate-pulse-fast skeleton-loader skeleton-sm w-48 mb-2" />
+                  <div className="animate-pulse-fast skeleton-loader skeleton-sm w-72 mb-5" />
+                </div>
+                <div className="animate-pulse-fast skeleton-loader skeleton-sm w-16 h-8" />
+              </div>
+              <div className="w-full h-80 mb-1.5 flex items-center justify-center">
+                <BarsScaleFade
+                  width="2rem"
+                  height="2rem"
+                  color="#6b7280"
+                  dur={0.5}
+                />
+              </div>
+            </div>
+          )}
+          {isSuccess && <SchoolOverview data={schoolChart} />}
+          {isError && (
+            <div className="bg-white p-5 rounded-xl dark:bg-gray-800">
+              <div className="mb-4">
+                <h5 className="font-semibold text-lg mb-0.5">
+                  School Overview
+                </h5>
+                <p className="text-gray-500">
+                  Overview of the school's performance.
+                </p>
+              </div>
+              <div className="w-full h-44 flex items-center justify-center flex-col">
+                <div className="mb-3">
+                  <AlertTriangleIcon
+                    size={42}
+                    className="stroke-gray-500 dark:stroke-gray-400"
+                  />
+                </div>
+                <p className="text-center text-gray-500">
+                  No data available. Please try again later.
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="col-span-full xl:col-span-2">
+          {isLoading && (
+            <div className="bg-white p-5 rounded-xl dark:bg-gray-800">
+              <div className="animate-pulse-fast skeleton-loader skeleton-sm w-36 mb-2" />
+              <div className="animate-pulse-fast skeleton-loader skeleton-sm w-3/4 min-w-40 mb-5" />
+              <div className="w-full h-80 mb-1.5 flex items-center justify-center">
+                <BarsScaleFade
+                  width="2rem"
+                  height="2rem"
+                  color="#6b7280"
+                  dur={0.5}
+                />
+              </div>
+            </div>
+          )}
+          {isSuccess && <GameOverview data={dashboards?.data?.games} />}
+          {isError && (
+            <div className="bg-white p-5 rounded-xl dark:bg-gray-800">
+              <div className="mb-4">
+                <h5 className="font-semibold text-lg mb-0.5">Game Overview</h5>
+                <p className="text-gray-500">
+                  Overview of the game's performance.
+                </p>
+              </div>
+              <div className="w-full h-44 flex items-center justify-center flex-col">
+                <div className="mb-3">
+                  <AlertTriangleIcon
+                    size={42}
+                    className="stroke-gray-500 dark:stroke-gray-400"
+                  />
+                </div>
+                <p className="text-center text-gray-500">
+                  No data available. Please try again later.
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
